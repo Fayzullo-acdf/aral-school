ad6c1ef9796b20c09c0ad2ebac1de13f440a3c3d
MacTavish27 <abduhakimovfayzullo@gmail.com>
2026-05-07 16:52:59 +0500

    Minor changes

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,52 @@
+@props(['menus', 'site_translations'])
+
+<section id="navbar">
+    <div class="container">
+        <div class="banner">
+            <div class="banner-textbox">
+                {!! $site_translations->banner_text ?? null !!}
+            </div>
+            <div class="lang-togglebox">
+                <ul>
+                    @foreach (LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                        <li>
+                            <a rel="alternate" hreflang="{{ $localeCode }}"
+                                href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}"
+                                class="{{ LaravelLocalization::getCurrentLocale() == $properties['locale_url'] ? 'selected' : null }}">
+                                {{ strtoupper($properties['locale_url']) }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+        <div class="navigation">
+            <div class="navigation_bottom">
+                <div class="aral">
+                    <a href="#">
+                        <img src="./logo/aral.svg" class="main_logo" alt="Aral" />
+                    </a>
+                    <a href="#">
+                        <img src="./logo/logo.svg" class="mobile_logo" alt="Aral" /></a>
+                </div>
+                <div class="navbar-square"></div>
+                <div class="menu">
+                    <ul>
+                        @foreach ($menus as $menu)
+                            <li>
+                                <a href="{{ $menu->url }}">{{ $menu->title }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="school">
+                    <a href="#"> <img src="./logo/SCHOOL.svg" alt="school" /></a>
+                </div>
+                <div class="hamburger_button" id="hamburger" role="button" tabindex="0" aria-controls="mobile-menu"
+                    aria-expanded="false">
+                    <div class="hamburger_rect"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="square"></div>
+</section>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,7 @@
     </div>
     <div class="overlay">
 
-        <x-head :menus="$menus" :site_translations="$site_translations" />
+        <x-header :site_translations="$site_translations" :menus="$menus" />
 
         @yield('main')
 
